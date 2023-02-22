7c039e7704144493e267d8a164ce9e79b46475a2
Jahnavi Cherala <jahnavi.cherala@gmail.com>
2023-02-23 01:19:57 +0530

    Movie labd update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=cbfdb2c0";
+
+function App() {
+  useEffect(() => {}, []);
+
+  return <div></div>;
+}
+
+export default App;
